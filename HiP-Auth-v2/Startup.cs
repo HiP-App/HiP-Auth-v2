87f5fb295ada3b314db9bdd4bfe2ac52c2ef71d2
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2017-04-16 13:29:35 +0200

    Configure database usage
    - use configuration & operational  stores from the database
    - populate the database on initial load
    - build the connection string from the appsettings

--- HiP-Auth-v2/Startup.cs
+++ HiP-Auth-v2/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,24 +34,71 @@ namespace PaderbornUniversity.SILab.Hip.Auth
 			services.AddMvc();
 
             var appConfig = new AppConfig(Configuration);
-            // configure identity server with in-memory stores, keys, clients and resources
-            services.AddIdentityServer()
-                .AddTemporarySigningCredential()
-                .AddInMemoryApiResources(AuthConfig.GetApiResources())
-                .AddInMemoryClients(AuthConfig.GetClients(appConfig))
-                .AddTestUsers(AuthConfig.GetUsers(appConfig))
-                .AddInMemoryIdentityResources(AuthConfig.GetIdentityResources());
+            services.AddSingleton<AppConfig>(appConfig);
+
+            // server=(localdb)\mssqllocaldb;database=IdentityServer4.Quickstart.EntityFramework;trusted_connection=yes";
+			var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+			// configure identity server with in-memory users, but EF stores for clients and resources
+			services.AddIdentityServer()
+				.AddTemporarySigningCredential()
+				.AddTestUsers(AuthConfig.GetUsers(appConfig))
+				.AddConfigurationStore(builder =>
+					builder.UseSqlServer(appConfig.DatabaseConfig.ConnectionString, options =>
+						options.MigrationsAssembly(migrationsAssembly)))
+				.AddOperationalStore(builder =>
+					builder.UseSqlServer(appConfig.DatabaseConfig.ConnectionString, options =>
+						options.MigrationsAssembly(migrationsAssembly)));
         }
 
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, AppConfig config)
         {
             loggerFactory.AddConsole(LogLevel.Debug);
             app.UseDeveloperExceptionPage();
+
+            InitializeDatabase(app, config);
 
             app.UseIdentityServer();
 
 			app.UseStaticFiles();
 			app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app, AppConfig config)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in AuthConfig.GetClients(config))
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in AuthConfig.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in AuthConfig.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
--- HiP-Auth-v2/Utility/AppConfig.cs
+++ HiP-Auth-v2/Utility/AppConfig.cs
@@ -1,20 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
 namespace PaderbornUniversity.SILab.Hip.Auth.Utility
 {
-    public class AppConfig
+  public class AppConfig
     {
         public AppConfig(IConfiguration configuration)
         {
+			DatabaseConfig = new DatabaseConfig
+			{
+				Host = configuration.GetValue<string>("DB_HOST"),
+				Username = configuration.GetValue<string>("DB_USERNAME"),
+				Password = configuration.GetValue<string>("DB_PASSWORD"),
+				Name = configuration.GetValue<string>("DB_NAME")
+			};
             Port = configuration.GetValue<int>("Port");
             User = configuration.GetValue<string>("User");
             UserPassword = configuration.GetValue<string>("User-Password");
             UserSecret = configuration.GetValue<string>("User-Secret");
         }
+
+		public DatabaseConfig DatabaseConfig { get; }
 
         public string UserPassword { get; set; }
 
@@ -24,4 +30,28 @@ namespace PaderbornUniversity.SILab.Hip.Auth.Utility
 
         public string UserSecret { get; set; }
     }
+
+	public class DatabaseConfig
+	{
+		public string Name { get; set; }
+
+		public string Host { get; set; }
+
+		public string Username { get; set; }
+
+		public string Password { get; set; }
+
+		public string ConnectionString
+		{
+			get
+			{
+				SqlConnectionStringBuilder connectionBuilder = new SqlConnectionStringBuilder();
+				connectionBuilder.DataSource = Host;
+				connectionBuilder.UserID = Username;
+				connectionBuilder.Password = Password;
+				connectionBuilder.InitialCatalog = Name;
+				return connectionBuilder.ConnectionString;
+			}
+		}
+	}
 }
