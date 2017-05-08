d9b4716e81d69aa994688430220bb9dda5f09eac
Janis Krasemann <janiskra@mail.uni-paderborn.de>
2017-05-08 14:03:26 +0200

    Manually set the service's port & url

--- AuthWithIdentity/Program.cs
+++ AuthWithIdentity/Program.cs
@@ -13,6 +13,7 @@ namespace PaderbornUniversity.SILab.Hip.Auth
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://localhost:5001")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
