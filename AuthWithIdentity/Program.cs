5a328a33086486c209857610ac4fd50995f44e0e
objectliteral <objectliteral@outlook.com>
2017-05-18 20:39:46 +0200

    Modified references to the database and fixed listening interface

--- AuthWithIdentity/Program.cs
+++ AuthWithIdentity/Program.cs
@@ -13,7 +13,7 @@ namespace PaderbornUniversity.SILab.Hip.Auth
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:5001")
+                .UseUrls("http://*:5001")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
