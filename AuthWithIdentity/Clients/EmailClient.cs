0eb7463da1546c062ff79e35b922b24782981bc9
Janis Krasemann <krasemann.janis@gmail.com>
2017-06-12 17:22:41 +0200

    Update mail service URI

--- AuthWithIdentity/Clients/EmailClient.cs
+++ AuthWithIdentity/Clients/EmailClient.cs
@@ -117,7 +117,7 @@ namespace PaderbornUniversity.SILab.Hip.Auth.Clients
         /// </summary>
         private void Initialize()
         {
-            BaseUri = new System.Uri("http://emailservice-develop");
+            BaseUri = new System.Uri("http://emailservice-develop-1");
             SerializationSettings = new JsonSerializerSettings
             {
                 Formatting = Newtonsoft.Json.Formatting.Indented,
